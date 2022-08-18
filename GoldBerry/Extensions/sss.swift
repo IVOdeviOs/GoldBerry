a0df2c731ca0f336dedc9f0978dd32e3de903b9a
Oleg Miatlitski <om114@tut.by>
2022-08-18 21:42:36 +0300

    ddd

---
+++ GoldBerry/Extensions/sss.swift
@@ -0,0 +1,20 @@
+//
+//  sss.swift
+//  GoldBerry
+//
+//  Created by олег Метлицкий on 18.08.22.
+//
+
+import SwiftUI
+
+struct sss: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct sss_Previews: PreviewProvider {
+    static var previews: some View {
+        sss()
+    }
+}
