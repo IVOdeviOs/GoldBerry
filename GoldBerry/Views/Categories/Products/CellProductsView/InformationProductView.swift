bf0a2e60484563e556880027748a260b7b4c8127
Vadim <ivo.dev.ios@gmail.com>
2022-08-20 23:39:32 +0300

    InformatiioonView

--- GoldBerry/Views/Categories/Products/CellProductsView/InformationProductView.swift
+++ GoldBerry/Views/Categories/Products/CellProductsView/InformationProductView.swift
@@ -23,12 +23,52 @@ struct InformationProductView: View {
                     .cornerRadius(30)
                     .padding(3)
                     Spacer()
+                    HStack {
+                        Image(systemName: "car")
+                            .resizable()
+                            .frame(width: 35, height: 35)
+                            .foregroundColor(Color.theme.gray)
+                        Text("Бесплатная доставка от ... руб")
+                            .font(.system(size: 16, weight: .medium, design: .default))
+                        Spacer()
+                    }
+                    .padding(13)
+                    .background(.gray.opacity(0.3))
+                    .cornerRadius(20)
+                    .padding(.horizontal)
+                    HStack {
+                        Text("1245₽")
+                            .foregroundColor(.red)
+                            .font(.system(size: 23, weight: .bold, design: .default))
+                        ZStack {
+                            Text("4500₽")
+                                .font(.system(size: 13, weight: .light, design: .serif))
+                                .foregroundColor(.black.opacity(0.6))
+                            Color(CGColor(gray: 0, alpha: 1)).opacity(0.5)
+                                .frame(width: 45, height: 0.5)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+
+                    HStack {
+                        Text("adsgkhagsdkha;sdgihg;asidgh;iagshd;ighda;siohg;asdoihg;aiosdhgahsdg;ioh;ashgaisdfsadfadsgadsgadsgadsgasdgadsgasadsgkhagsdkha;sdgihg;asidgh;iagshd;ighda;siohg;asdoihg;aiosdhgahsdg;ioh;ashgaisdfsadfadsgadsgadsgadsgasdgadsgashgohgoadsgkhagsdkha;sdgihg;asidgh;iagshd;ighda;siohg;asdoihg;aiosdhgahsdg;ioh;ashgaisdfsadfadsgadsgadsgadsgasdgadsgasadsgkhagsdkha;sdgihg;asidgh;iagshd;ighda;siohg;asdoihg;aiosdhgahsdg;ioh;ashgaisdfsadfadsgadsgadsgadsgasdgadsgashgohgo")
+                            .font(.system(size: 16, weight: .medium, design: .default))
+                        Spacer()
+                    }
+
+                    .frame(minHeight: 70, maxHeight: 150)
+                    .padding(.horizontal)
+                    
+                   
+//                    .padding(.vertical,2)
                 }
-                .offset(y: -20)
+                .offset(y: -35)
                 .navigationBarHidden(true)
             }
 
             ZStack {
+                VStack{
                 HStack {
                     Button {
                         dismiss()
@@ -57,6 +97,28 @@ struct InformationProductView: View {
                 }
                 .padding(.horizontal)
 //                .padding(.top, 1)
+                    Spacer()
+                VStack {
+                    Button {
+                        print("🥶")
+                    } label: {
+                        HStack {
+                            Text("В корзину")
+                                .foregroundColor(.white)
+                                .font(.system(size: 18, weight: .bold, design: .serif))
+                            Spacer()
+                            Text("1245₽")
+                                .foregroundColor(.white)
+                                .font(.system(size: 18, weight: .bold, design: .serif))
+                        }
+                        .padding()
+                        .background(Color.theme.lightGreen)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 16)
+                    }
+                }
+                .offset(y:-75)
+            }
             }
         }
     }
