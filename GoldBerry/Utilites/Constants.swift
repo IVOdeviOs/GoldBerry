c58e26109603d5cb557b2c46b308e2418e370a16
Vadim <ivo.dev.ios@gmail.com>
2022-09-03 20:40:26 +0300

    s

--- GoldBerry/Utilites/Constants.swift
+++ GoldBerry/Utilites/Constants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum Constants {
 //    static let baseURL = "http://127.0.0.1:8080/"
-    static let baseURL = "https://df6e-37-214-57-113.eu.ngrok.io/"
+    static let baseURL = "https://17c4-37-214-78-38.eu.ngrok.io/"
 
 
 }
