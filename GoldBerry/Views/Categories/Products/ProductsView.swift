677051ec2b643d8138838dc5839f148afde5b6dd
Oleg Miatlitski <om114@tut.by>
2022-09-13 22:33:23 +0300

    allFru

--- GoldBerry/Views/Categories/Products/ProductsView.swift
+++ GoldBerry/Views/Categories/Products/ProductsView.swift
@@ -11,7 +11,7 @@ enum categories: String {
 struct ProductsView: View {
     @ObservedObject var viewModel: FruitViewModel
 
-    @State var tag = ""
+    @State var tag = "allFruits"
 
     var body: some View {
 
